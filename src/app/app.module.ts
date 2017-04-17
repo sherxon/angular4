e97f81979abb9d1833058460bf0e5d7f812933d3
sherxon <sheraliobidov1199@gmail.com>
2017-04-17 18:41:37 -0500

    components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+/**
+ * Created by sherxon on 4/17/17.
+ */
+@Component({
+  selector : 'app-server',
+  templateUrl : './server.component.html',
+})
+export class ServerComponent{
+}
